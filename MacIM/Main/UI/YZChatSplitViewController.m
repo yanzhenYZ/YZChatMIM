6c6d0dab4c0c5bcea6d60d75b02fb2625e0ff8cf
yanzhen <yanzhen@meishubao.com>
2020-09-26 15:30:21 +0800

    add splite

---
+++ MacIM/Main/UI/YZChatSplitViewController.m
@@ -0,0 +1,21 @@
+//
+//  YZChatSplitViewController.m
+//  MacIM
+//
+//  Created by yanzhen on 2020/9/26.
+//
+
+#import "YZChatSplitViewController.h"
+
+@interface YZChatSplitViewController ()
+
+@end
+
+@implementation YZChatSplitViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
